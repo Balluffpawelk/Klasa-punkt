2cf2d4cbc61e9217effd0db3f34d0589c447ab9f
Paweł Kwacz <pawel.kwacz@balluff.pl>
2018-09-26 09:37:09 +0200

    Dodaj pliki projektów.

---
+++ Klasa punkt/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasa_punkt
+{
+    class Program
+    {
+        public class Punkt
+        {
+            public int x;
+            public int y;
+            public void Wyswietl()
+            {
+                Console.WriteLine("Aktualne współrzędne punktu: x = {0}, y={1}", x, y);
+                Console.ReadKey();
+            }
+
+        }
+
+            static void Main(string[] args)
+        {
+            string Aa;
+            string Bb;
+            string oper;
+            int przesun = 0;
+            int ile = 0;
+            string ile_s;
+            
+            Punkt start1 = new Punkt();
+
+            Console.WriteLine("Podaj współrzędną x: \n" );
+            Aa = Console.ReadLine();
+            start1.x = Int32.Parse(Aa);
+         
+            Console.WriteLine("Podaj współrzędną y: \n");
+            Bb = Console.ReadLine();
+            start1.y = Int32.Parse(Bb);
+            start1.Wyswietl();
+
+            Console.WriteLine("Czy chcesz przesunąć punkt?\n 0- Nie \n 1- W górę\n 2- W dół\n 3- W lewo\n 4- W prawo\n");
+            oper = Console.ReadLine();
+            przesun = Int32.Parse(oper);
+            
+            if (przesun>0)
+            {
+                    Console.WriteLine("O ile chcesz przesunąć punkt\n");
+                    ile_s = Console.ReadLine();
+                    ile = Int32.Parse(ile_s);
+            }
+            
+
+
+            switch (przesun)
+            {
+                case 0:
+                    Console.WriteLine("Współrzędne pozostają bez zmian.\n");
+                    break;
+
+                case 1:
+                    start1.y = start1.y + ile;
+                    break;
+                case 2:
+                    start1.y = start1.y - ile;
+                    break;
+                case 3:
+                    start1.x = start1.x - ile;
+                    break;
+                case 4:
+                    start1.x = start1.x + ile;
+                    break;
+
+            }
+            start1.Wyswietl();
+
+        }
+    }
+
+
+    
+
+    
+}
---
+++ Klasa punkt/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("Klasa punkt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Balluff GmbH")]
+[assembly: AssemblyProduct("Klasa punkt")]
+[assembly: AssemblyCopyright("Copyright © Balluff GmbH 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("ac5c2b58-3dc4-4e6a-a1d4-7e037d7b37c3")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Rewizja
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
